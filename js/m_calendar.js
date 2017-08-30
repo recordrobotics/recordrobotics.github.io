9ce4c52147f6f0b1aadc8a95d81436a1cba9f625
fingoldin <vassilioskaxiras@gmail.com>
2017-08-31 02:06:43 +0300

    updated pages

--- js/m_calendar.js
+++ js/m_calendar.js
@@ -1,41 +1,36 @@
 
 class Calendar
 {
-	constructor(e, docs, id, key, scope, url)
+	constructor(e, docs, key, url)
 	{
 		this.parentE = e;
 		this.discovery_docs = docs;
-		this.client_id = id;
 		this.api_key = key;
-		this.scopes = scope;
 		this.calendar_id = url;
 
 		this.parentE.html("<div class='wheel-loader-wrap'><div class='wheel-loader'></div></div>");
 
-		gapi.load('client:auth2', function() {
-	        	gapi.client.init({
+		gapi.load('client', function() {
+	       		gapi.client.init({
 	        		discoveryDocs: docs,
-	        		clientId: id,
-				apiKey: key,
-	          		scope: scope
-	  		});
+				apiKey: key
+	  		}).then(function() {
+				setTimeout(function() {
+					$(e).html("").load("/util/m_calendar.html", function() {
+						console.log("loaded");
+						gapi.client.calendar.events.list({
+		        				'calendarId': url //'belmontschools.net_qucdk22mq5p84o573a88motvpk@group.calendar.google.com'
+		        			}).then(function(response) {
+		          				var events = response.result.items;
 
-			setTimeout(function() {
-				$(e).html("").load("/util/m_calendar.html", function() {
-					console.log("loaded");
-					gapi.client.calendar.events.list({
-		        			'calendarId': url //'belmontschools.net_qucdk22mq5p84o573a88motvpk@group.calendar.google.com'
-		        		}).then(function(response) {
-		          			var events = response.result.items;
+							console.log(events);
 
-						console.log(events);
-
-						//events.each(function() {
-
-						//});
+							//events.each(function() {
+							//});
+						});
 					});
-				});
-			}, 2000);
+				}, 1000);
+			});
 		});
 	}
 }
